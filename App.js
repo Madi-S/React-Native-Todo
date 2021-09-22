ef71f852bf3c738d5a746d3cab0618224db2c91e
Madi-S <khovansky99@gmail.com>
2021-09-22 16:51:52 +0600

    Played with styles a bit

--- App.js
+++ App.js
@@ -13,10 +13,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 1, // means take all space
+        // height: 200, // not recommended due to variety of resolutions
+        flexDirection: 'column',
         backgroundColor: '#000',
-        alignItems: 'center',
-        justifyContent: 'center'
+        alignItems: 'center', // horizontal alignment (for column, vice versa for row direction)
+        justifyContent: 'center' // vertical alignment (for column, vice versa for row direction)
     },
     text: {
         color: '#fff',
